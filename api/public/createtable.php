734e6aae3cb6ae0f91c216a602a91af458f95afd
Sufiyan Shaikh <sufiyanshaikh7698@gmail.com>
2026-07-26 01:44:48 +0530

    Create createtable.php

---
+++ api/public/createtable.php
@@ -0,0 +1,53 @@
+<?php
+
+require_once __DIR__ . "/../helpers/response.php";
+
+class Database
+{
+    private $conn;
+
+    public function connect()
+    {
+        $this->conn = null;
+
+        try {
+
+            $host = "mysql.railway.internal";
+            $port = "3306";
+            $db   = "railway";
+            $user = "root";
+            $pass = "KInyvkFHRxEzXDzBEmQbDXoRvyyNitaz";
+
+            $dsn = "mysql:host={$host};port={$port};dbname={$db};charset=utf8mb4";
+
+            $this->conn = new PDO(
+                $dsn,
+                $user,
+                $pass
+            );
+
+            $this->conn->setAttribute(
+                PDO::ATTR_ERRMODE,
+                PDO::ERRMODE_EXCEPTION
+            );
+
+            $this->conn->setAttribute(
+                PDO::ATTR_DEFAULT_FETCH_MODE,
+                PDO::FETCH_ASSOC
+            );
+
+        } catch (PDOException $e) {
+
+            response(
+                false,
+                "Database Connection Failed",
+                [
+                    "error" => $e->getMessage()
+                ]
+            );
+
+        }
+
+        return $this->conn;
+    }
+}
