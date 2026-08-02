2870fa97e14cbf945a18f4855cb2c74fe629b5b9
Sufiyan Shaikh <sufiyanshaikh7698@gmail.com>
2026-08-02 20:59:41 +0530

    Updation of SuperAdmin

--- api/public/createtable.php
+++ api/public/createtable.php
@@ -8,32 +8,7 @@ try {
     $database = new Database();
     $db = $database->connect();
 
-    $sql = "CREATE TABLE backup_settings (
-
-    id INT PRIMARY KEY,
-
-    retention_days INT DEFAULT 90,
-
-    auto_backup TINYINT(1) DEFAULT 1,
-
-    google_drive_sync TINYINT(1) DEFAULT 0,
-
-    compress_backup TINYINT(1) DEFAULT 1,
-
-    backup_time TIME DEFAULT '02:00:00'
-
-);
-
-INSERT INTO backup_settings
-VALUES
-(
-1,
-90,
-1,
-0,
-1,
-'02:00:00'
-);";
+    $sql = "DROP TABLE backup_history;";
 
     $db->exec($sql);
 
